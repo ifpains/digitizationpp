53dfa4aab0f2c2326b3575cfe2478a685f6e3d61
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2025-12-04 08:23:50 +0000

    a version that submits jobs

--- test/submit_digi.py
+++ test/submit_digi.py
@@ -12,12 +12,12 @@ export IMAGE="/cvmfs/sft-cygno.infn.it/dockers/images/cygno-wn_v2.4.sif"
 source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-ubuntu2204-gcc11-opt/setup.sh
 source /cvmfs/sft-cygno.infn.it/config/setup_digi.sh
 cd WORKDIR
-EXECCOMMAND
-source $CVMFS_PARENT_DIR/cvmfs/sft-cygno.infn.it/config/cygno_htc -s $CONFOUTPATH/sub $CE
+COMMAND
+source $CVMFS_PARENT_DIR/cvmfs/sft-cygno.infn.it/config/cygno_htc -s  $CE
 '''
 
 
-def writeCondorCfg(jobdir, srcFiles, options, logdir, errdir, outdirCondor):
+def makeCondorFile(jobdir, srcFiles, options, logdir, errdir, outdirCondor):
 
     dummy_exec = open(jobdir+'/dummy_exec.sh','w')
     dummy_exec.write('#!/bin/bash\n')
@@ -26,29 +26,34 @@ def writeCondorCfg(jobdir, srcFiles, options, logdir, errdir, outdirCondor):
      
     condor_file_name = jobdir+'/condor_submit.condor'
     condor_file = open(condor_file_name,'w')
-    ondor_file.write('''Universe = vanilla
-    Executable = {de}
-    Log        = {ld}/$(ProcId).log
-    Output     = {od}/$(ProcId).out
-    Error      = {ed}/$(ProcId).error
-    getenv      = True
-    next_job_start_delay = 1
-    environment = "LS_SUBCWD={here}"
-    equest_cpus = {cpu}
-    +CygnoUser = "{user}\n"
-    '''.format(de=os.path.abspath(dummy_exec.name), ld=os.path.abspath(logdir), od=os.path.abspath(outdirCondor),ed=os.path.abspath(errdir),
-               cpu=cpu, user=os.environ['USER'], here=os.environ['PWD'] ) )
+    condor_file.write('''Universe = vanilla
+Executable = {de}
+Log        = {ld}/$(ProcId).log
+Output     = {od}/$(ProcId).out
+Error      = {ed}/$(ProcId).error
+getenv      = True
+next_job_start_delay = 1
+environment = "LS_SUBCWD={here}"
+request_cpus = {cpu}
++CygnoUser = "{user}"\n
+'''.format(de=os.path.abspath(dummy_exec.name), ld=os.path.abspath(logdir), od=os.path.abspath(outdirCondor),ed=os.path.abspath(errdir),
+               cpu=options.threads, user=os.environ['USERNAME'], here=os.environ['PWD'] ) )
+    for sf in srcFiles:
+        condor_file.write(f'arguments = {os.path.abspath(sf)} \nqueue 1 \n\n')
+    condor_file.close()
     return condor_file_name
 
-def replaceParam(config_txt,old_string,new_string):
+def replaceParam(input_file,old_string,new_string,output_file=None):
     with open(input_file, "r", encoding="utf-8") as f:
         lines = f.readlines()
     new_lines = [line.replace(old_string, new_string) for line in lines]
 
+    if not output_file:
+        output_file = input_file
     with open(output_file, "w", encoding="utf-8") as f:
         f.writelines(new_lines)
 
-print(f"Replaced '{old_string}' with '{new_string}' in {output_file}")
+    print(f"Replaced {old_string} with {new_string} in {output_file}")
 
 if __name__ == "__main__":
 
@@ -71,7 +76,7 @@ if __name__ == "__main__":
         if not os.path.isdir(absopath):
             print ('making a directory and running in it')
             os.system('mkdir -p {od}'.format(od=absopath))
-
+    
     jobdir = absopath+'/jobs/'
     if not os.path.isdir(jobdir):
         os.system('mkdir {od}'.format(od=jobdir))
@@ -84,20 +89,34 @@ if __name__ == "__main__":
     outdirCondor = absopath+'/outs/'
     if not os.path.isdir(outdirCondor):
         os.system('mkdir {od}'.format(od=outdirCondor))
+    basedigidir = os.path.abspath(args.executable).split('build-dir')[0]
+    os.system(f"cp -r {basedigidir}/VignettingMap {jobdir}")
 
     srcfiles = []
     for a,alpha in enumerate(args.alphas):
         for g,gain in enumerate(args.gains):
-            print ("Prepare job for pair (alpha,normgain) = (f"{alpha}","{gain}")")
-            con_file_name = jobdir+f"/conf_"{a}"-"{g}".txt"
-            job_file_name = jobdir+f"/job_"{a}"-"{g}".sh"
-            log_file_name = logdir+f"/job_"{a}"-"{g}".sh"
+            print (f"Prepare job for pair (alpha,normgain) = ({alpha},{gain})")
+            con_file_name = jobdir+f"/conf_{a}-{g}.txt"
+            os.system(f"cp {args.config} {con_file_name}") 
+            job_file_name = jobdir+f"/job_{a}-{g}.sh"
+            log_file_name = logdir+f"/job_{a}-{g}.sh"
             tmp_file = open(job_file_name, 'w')
 
-            replaceParam(con_file_name,"'c_G'                   : 0.03",f"'c_G'                   : "{gain:.3f}" ")
-            replaceParam(con_file_name,"alpha_G'               : 0.0209",f"'alpha_G'                   : "{alpha:.3f}" ")
+            replaceParam(con_file_name,"'c_G'                   : 0.03",  f"'c_G'                   : {gain:.3f}")
+            replaceParam(con_file_name,"'alpha_G'               : 0.0209",f"'alpha_G'               : {alpha:.3f}")
             
             tmp_filecont = jobstring
-            cmd = ""
+            tmp_filecont = tmp_filecont.replace('WORKDIR',jobdir)
+            # N.B.: use explicitly "./" as the directory for the config file name, because the path for the vignetting is built from there in DigitizationRunner.cxx
+            cmd = f"{os.path.abspath(args.executable)} ./{os.path.basename(con_file_name)} -I {os.path.abspath(args.inputdir)} -O {os.path.abspath(args.outdir)}/digi_{a}-{g}/"
+            tmp_filecont = tmp_filecont.replace('COMMAND',cmd)
+            tmp_file.write(tmp_filecont)
+            tmp_file.close()
+            srcfiles.append(job_file_name)
+    cf = makeCondorFile(jobdir,srcfiles,args,logdir,errdir,outdirCondor)
+    subcmd = f'source $CVMFS_PARENT_DIR/cvmfs/sft-cygno.infn.it/config/cygno_htc -s {cf} 2'
 
-            cmd = f" {args.executable} ../digitizationpp/config/ConfigFile_new.txt -I input_fe_zsteps -O output_fe_steps
+    print (subcmd)
+
+    sys.exit()
+    
