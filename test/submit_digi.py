f17457c81cdab316aa9442380ce19c5438631aa3
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2025-12-10 17:04:03 +0000

    a first working version (not fully tested) of the batch submission for digis

--- test/submit_digi.py
+++ test/submit_digi.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 import os, sys
 
-jobstring  = '''#!/bin/sh
+jobstring  = '''#!/bin/bash
 ulimit -c 0 -S
 ulimit -c 0 -H
 set -e
-export CE=2
-export IMAGE="/cvmfs/sft-cygno.infn.it/dockers/images/cygno-wn_v2.4.sif"
 
-# Experimet executable config
+# Experiment executable config
+export CVMFS_PARENT_DIR=""
 source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-ubuntu2204-gcc11-opt/setup.sh
 source /cvmfs/sft-cygno.infn.it/config/setup_digi.sh
-cd WORKDIR
 COMMAND
-source $CVMFS_PARENT_DIR/cvmfs/sft-cygno.infn.it/config/cygno_htc -s  $CE
 '''
 
 
-def makeCondorFile(jobdir, srcFiles, options, logdir, errdir, outdirCondor):
+def makeCondorFile(jobdir, srcFiles, cfgFiles, outFiles, options, logdir, errdir, outdirCondor):
 
     dummy_exec = open(jobdir+'/dummy_exec.sh','w')
     dummy_exec.write('#!/bin/bash\n')
@@ -26,7 +23,10 @@ def makeCondorFile(jobdir, srcFiles, options, logdir, errdir, outdirCondor):
      
     condor_file_name = jobdir+'/condor_submit.condor'
     condor_file = open(condor_file_name,'w')
-    condor_file.write('''Universe = vanilla
+    condor_file.write('''+SingularityImage = "/cvmfs/sft-cygno.infn.it/dockers/images/cygno-wn_v2.4.sif"
++SingularityBind = "/cvmfs/:/cvmfs/"
+Requirements = HasSingularity
+
 Executable = {de}
 Log        = {ld}/$(ProcId).log
 Output     = {od}/$(ProcId).out
@@ -35,11 +35,21 @@ getenv      = True
 next_job_start_delay = 1
 environment = "LS_SUBCWD={here}"
 request_cpus = {cpu}
+should_transfer_files   = YES
+preserve_relative_paths = True
 +CygnoUser = "{user}"\n
-'''.format(de=os.path.abspath(dummy_exec.name), ld=os.path.abspath(logdir), od=os.path.abspath(outdirCondor),ed=os.path.abspath(errdir),
+'''.format(de=dummy_exec.name,
+           ld=os.path.abspath(logdir), od=os.path.abspath(outdirCondor),ed=os.path.abspath(errdir),
                cpu=options.threads, user=os.environ['USERNAME'], here=os.environ['PWD'] ) )
-    for sf in srcFiles:
-        condor_file.write(f'arguments = {os.path.abspath(sf)} \nqueue 1 \n\n')
+    for i,sf in enumerate(srcFiles):
+        condor_file.write(f'transfer_input_files = {os.path.abspath(options.srcdir)}/build-dir,{os.path.abspath(options.srcdir)}/VignettingMap,{os.path.abspath(cfgFiles[i])},{os.path.abspath(options.inputdir)},{os.path.abspath(sf)} \n')
+        condor_file.write(f'transfer_output_files = {os.path.splitext(outFiles[i])[0]}\n')
+        if options.storagedir:
+            condor_file.write(f'transfer_output_remaps = "{os.path.splitext(outFiles[i])[0]} = {options.storagedir}/{os.path.splitext(outFiles[i])[0]}"\n')
+        else:
+            condor_file.write(f'transfer_output_remaps = "{os.path.splitext(outFiles[i])[0]} = {outdirCondor}/{os.path.splitext(outFiles[i])[0]}"\n')
+        condor_file.write(f'arguments = {os.path.basename(sf)} \nqueue \n\n')
+        
     condor_file.close()
     return condor_file_name
 
@@ -59,13 +69,14 @@ if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("executable", help="digitization exe file")
-    parser.add_argument("config", help="digitization config file")
+    parser.add_argument("srcdir", help="base directory where build-dir is")
     parser.add_argument("-a", "--alphas", type=float, nargs="*",  default=[0.01, 0.02, 0.03], help="List of alpha values to scan (default: %(default)s)")
     parser.add_argument("-g", "--gains", type=float, nargs="*",  default=[0.02, 0.03, 0.04], help="List of gain normalization values to scan (default: %(default)s)")
+    parser.add_argument("-c", "--ce", type=int, default=2, help="Computing element in condor to use")
     parser.add_argument("-t", "--threads", type=int, default=8, help="Number of CPUs to request")
     parser.add_argument("-o", "--outdir", type=str, default=None, help='output directory');
     parser.add_argument("-i", "--inputdir", type=str, default=None, help='input directory');
+    parser.add_argument("-s", "--storagedir", type=str, default=None, help='output directory for the data (if null, use local outdir)');
     args = parser.parse_args()
 
     print("SUBMIT DIGI")
@@ -79,42 +90,53 @@ if __name__ == "__main__":
     
     jobdir = absopath+'/jobs/'
     if not os.path.isdir(jobdir):
-        os.system('mkdir {od}'.format(od=jobdir))
+        os.system('mkdir -p {od}'.format(od=jobdir))
+        os.system(f'chmod 777 {jobdir}')
     logdir = absopath+'/logs/'
     if not os.path.isdir(logdir):
-        os.system('mkdir {od}'.format(od=logdir))
+        os.system('mkdir -p {od}'.format(od=logdir))
+        os.system(f'chmod 777 {logdir}')
     errdir = absopath+'/errs/'
     if not os.path.isdir(errdir):
-        os.system('mkdir {od}'.format(od=errdir))
+        os.system('mkdir -p {od}'.format(od=errdir))
+        os.system(f'chmod 777 {errdir}')
     outdirCondor = absopath+'/outs/'
     if not os.path.isdir(outdirCondor):
-        os.system('mkdir {od}'.format(od=outdirCondor))
-    basedigidir = os.path.abspath(args.executable).split('build-dir')[0]
-    os.system(f"cp -r {basedigidir}/VignettingMap {jobdir}")
+        os.system('mkdir -p {od}'.format(od=outdirCondor))
+        os.system(f'chmod 777 {outdirCondor}')
+    if not args.storagedir:
+        outdirStorage = outdirCondor
+    else:
+        outdirStorage = os.path.abspath(args.storagedir)
 
-    srcfiles = []
+    srcfiles,cfgfiles,outfiles = [],[],[]
     for a,alpha in enumerate(args.alphas):
         for g,gain in enumerate(args.gains):
             print (f"Prepare job for pair (alpha,normgain) = ({alpha},{gain})")
             con_file_name = jobdir+f"/conf_{a}-{g}.txt"
-            os.system(f"cp {args.config} {con_file_name}") 
+            os.system(f"cp {args.srcdir}/config/ConfigFile_new.txt {con_file_name}") 
             job_file_name = jobdir+f"/job_{a}-{g}.sh"
             log_file_name = logdir+f"/job_{a}-{g}.sh"
             tmp_file = open(job_file_name, 'w')
 
             replaceParam(con_file_name,"'c_G'                   : 0.03",  f"'c_G'                   : {gain:.3f}")
             replaceParam(con_file_name,"'alpha_G'               : 0.0209",f"'alpha_G'               : {alpha:.3f}")
+            replaceParam(con_file_name,"'events'                : -1",f"'events'                : 10")
             
             tmp_filecont = jobstring
-            tmp_filecont = tmp_filecont.replace('WORKDIR',jobdir)
             # N.B.: use explicitly "./" as the directory for the config file name, because the path for the vignetting is built from there in DigitizationRunner.cxx
-            cmd = f"{os.path.abspath(args.executable)} ./{os.path.basename(con_file_name)} -I {os.path.abspath(args.inputdir)} -O {os.path.abspath(args.outdir)}/digi_{a}-{g}/"
+            cmd = f"./build-dir/digitizationpp ./{os.path.basename(con_file_name)} -I {os.path.basename(os.path.normpath(args.inputdir))} -O digi_{a}-{g}"
             tmp_filecont = tmp_filecont.replace('COMMAND',cmd)
             tmp_file.write(tmp_filecont)
             tmp_file.close()
             srcfiles.append(job_file_name)
-    cf = makeCondorFile(jobdir,srcfiles,args,logdir,errdir,outdirCondor)
-    subcmd = f'source $CVMFS_PARENT_DIR/cvmfs/sft-cygno.infn.it/config/cygno_htc -s {cf} 2'
+            cfgfiles.append(con_file_name)
+            targetDir = outdirCondor if not args.storagedir else outdirStorage
+            os.system(f'mkdir -p {targetDir}/digi_{a}-{g}')
+            os.system(f'chmod 777 {targetDir}/digi_{a}-{g}')
+            outfiles.append(f'digi_{a}-{g}')
+    cf = makeCondorFile(jobdir,srcfiles,cfgfiles,outfiles,args,logdir,errdir,outdirCondor)
+    subcmd = f'source $CVMFS_PARENT_DIR/cvmfs/sft-cygno.infn.it/config/cygno_htc -s {cf} {args.ce}'
 
     print (subcmd)
 
